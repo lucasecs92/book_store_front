c401b41b9307b65ba61c037809c834096fdd125b
Lucas Eduardo <102336205+lucasecs92@users.noreply.github.com>
2023-10-19 17:03:10 -0300

    update

--- book-store/src/components/NavBar.tsx
+++ book-store/src/components/NavBar.tsx
@@ -1,17 +1,34 @@
-import './NavBar.module.css'
+import { MagnifyingGlass } from '@phosphor-icons/react'
+import styles from './NavBar.module.css'
 
 export function NavBar() {
     return (
-        <nav>
-            <a href="#">Book Store</a>
-            <div>
-                <input type="text" />
-                <button type="submit">
-                    Pesquisar
+        <nav className={styles.navbarMain}>
+            <section className={styles.navLeft}>
+                <a className={styles.navLogo} href="#">Book store</a>
+            </section>
+            <form className={styles.navSearchForm}>
+                <select className={styles.searchDropdown}>
+                    <option value="Todos">Todos</option>
+                    <option value="Livros">Livros</option>
+                    <option value="Hq">Quadrinhos</option>
+                    <option value="Ebooks">eBooks</option>
+                </select>
+                <input 
+                    className={styles.searchField}
+                    type="text"  
+                    placeholder="O que você está buscando?"
+                />
+                <button className={styles.searchBtn} type="submit">
+                    <MagnifyingGlass />
                 </button>
-            </div>
+            </form>
+            <section className={styles.navRight}>
+                <p>Login</p>
+                <p>CART</p>
+            </section>
         </nav>      
     )
 }
-
+    
 
