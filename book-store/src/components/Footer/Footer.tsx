e3608f3c7fb3f2b694a0b3063ccd584e8689f78e
Lucas Eduardo <102336205+lucasecs92@users.noreply.github.com>
2023-10-23 00:16:40 -0300

    rodapé quase feito

--- book-store/src/components/Footer/Footer.tsx
+++ book-store/src/components/Footer/Footer.tsx
@@ -1,13 +1,29 @@
+import { FacebookLogo, InstagramLogo, PinterestLogo, YoutubeLogo } from '@phosphor-icons/react';
 import styles from './Footer.module.css';
 
 export function Footer() {
     return (
-        <section>
+        <section className={styles.footer}>
             <section className={styles.dividingLine}>
-
             </section>
-            <section className={styles.footer}>
 
+            <section className={styles.mainFooter}>
+                <section className={styles.leftFooter}>
+                    <a className={styles.logo} href="#">Book store</a>
+                </section>
+                <aside className={styles.rightFooter}>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam id ea vero, saepe, optio itaque quia fugiat, exercitationem eveniet necessitatibus velit! Corporis non vitae voluptate accusamus deleniti harum exercitationem officia!</p>
+                </aside>
+            </section>
+
+            <section className={styles.secondaryFooter}>
+                <p>Copyright (c) 2023 Lucas Eduardo</p>
+                <section className={styles.socialLogos}>
+                    <a className={styles.socialItem} href="#"><FacebookLogo /></a>
+                    <a className={styles.socialItem} href="#"><YoutubeLogo /></a>
+                    <a className={styles.socialItem} href="#"><InstagramLogo /></a>
+                    <a className={styles.socialItem} href="#"><PinterestLogo /></a>
+                </section>
             </section>
         </section>
     )
